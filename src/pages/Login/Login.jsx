1b538233e0425efb477a222191510734af89bc74
MdRaihan3 <1mdr91012@gmail.com>
2024-04-05 16:02:23 +0600

    first commit

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div>
+            <h1 className=" text-3xl text-center">Login</h1>
+            <p className=" text-3xl">This is login</p>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Login;
